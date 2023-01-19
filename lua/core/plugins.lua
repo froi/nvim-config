24bee258fbcc33eacaefc6e61d132981fc99af00
Froilán Irizarry Rivera <1918027+froi@users.noreply.github.com>
2023-01-18 23:39:45 -0500

    Add markdown preview plugin
    I've added Glow to my list of plugins. While not the best markdown
    preview I've seen, it'll be good enough for a quick preview.

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -53,6 +53,9 @@ return require('packer').startup(function(use)
   use "lewis6991/gitsigns.nvim"
 
   use "akinsho/toggleterm.nvim"
+
+  use {"ellisonleao/glow.nvim"}
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
