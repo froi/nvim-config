637abc71c3cea6dd56c07aa6fdfcd29ca8acb80f
Froilán Irizarry Rivera <1918027+froi@users.noreply.github.com>
2023-01-22 19:31:39 -0500

    Add new schemas
    I've added schemas to LSP completions for GitHub Actions and CircleCI
    yaml config files.

---
+++ lua/plugins/lsp/server_settings/yamlls.lua
@@ -0,0 +1,8 @@
+return {
+	settings = {
+    schemas = {
+      ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+      ["https://json.schemastore.org/circleciconfig.json"] = "/.circleci/*"
+    }
+	}
+}
